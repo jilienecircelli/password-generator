16b67371a766ac712fbc1d6289d81097c150a6e0
Jiliene Circelli <jilienecircelli@gmail.com>
2019-10-05 20:58:54 -0700

    updated js file and index

--- javascript.js
+++ javascript.js
@@ -1,32 +1,43 @@
-document.querySelector("#generate").onclick = function generatePassword() {
+document.querySelector("#generate").addEventListener("click", function() {
     var numberOfChar = prompt("How many characters do you want the password to be?")
+    var hasSpecialChar = confirm("Do you want special characters in the password?")
+    var hasNumericChar = confirm("Do you want numbers in the password?")
+    var hasUpperChar = confirm("Do you want upper case letters in the password?")
+    var hasLowerChar = confirm("Do you want lower case letters in the password?")
 
-    var specialChar = confirm("Do you want special characters in the password?")
-    var numeric = confirm("Do you want numbers in the password?")
-    var upperCase = confirm("Do you want upper case letters in the password?")
-    var lowerCase = confirm("Do you want lower case letters in the password?")
+    genPassword(numberOfChar, hasSpecialChar, hasNumericChar, hasUpperChar, hasLowerChar)
 
-    var special = ["!@#$%^&*()+=.~"]
-    var numbers = ["1234567890"]
-    var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-    var lower = ["abcdefghijklmnopqrstuvwxyz"]
+})
 
-    if (specialChar === true) {
-        for (var i = 0; i < special.length; i++) {
-            var passReturn = Math.floor((Math.random) * special.length)
-            document.querySelector("placeholder").replaceWith(passReturn)
-        }
-    }
+var special = "!@#$%^&*()+=.~".split("");
+var numbers = "1234567890".split("");
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
 
+function getRandomValue(arr) {
+    var randomIndex = Math.floor((Math.random) * arr.length)
+    return arr[randomIndex]
 }
 
-generatePassword()
+function genPassword(numberOfChar, hasSpecialChar, hasNumericChar, hasUpperChar, hasLowerChar) {
+    var randomSpecialChar;
+    var randomNumericChar;
+    var randomUpperChar;
+    var randomLowerChar;
 
+    if (hasSpecialChar) {
+        randomSpecialChar = getRandomValue(special)
+    }
+    if (hasNumericChar) {
+        randomNumericChar = getRandomValue(numbers)
+    }
+    if (hasUpperChar) {
+        randomUpperChar = getRandomValue(upper)
+    }
+    if (hasLowerChar) {
+        randomLowerChar = getRandomValue(lower)
+    }
+    var newChar = (randomSpecialChar + randomNumericChar + randomUpperChar + randomLowerChar)
+    console.log(newChar)
 
-
-// document.querySelector("#copy").onclick = function copy() {
-//     var copyText = document.getElementById("#password").select;
-//     document.execCommand("copy");
-
-//     alert("You've copied " + generatePassword + "to your clipboard!")
-// }
+}
